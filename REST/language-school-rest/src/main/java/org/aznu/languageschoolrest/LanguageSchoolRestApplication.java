197ff06e7b758f3ddb83b106eabd1fa643f96674
gaussx <kmalecki2002@gmail.com>
2025-12-16 10:42:56 +0100

    Add REST app template

---
+++ REST/language-school-rest/src/main/java/org/aznu/languageschoolrest/LanguageSchoolRestApplication.java
@@ -0,0 +1,13 @@
+package org.aznu.languageschoolrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageSchoolRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageSchoolRestApplication.class, args);
+    }
+
+}
---
+++ REST/language-school-rest/src/test/java/org/aznu/languageschoolrest/LanguageSchoolRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aznu.languageschoolrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageSchoolRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
